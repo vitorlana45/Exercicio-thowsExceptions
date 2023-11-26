078625705b5a1faeb61e81c40a85f744959171a0
vitor lana <vitoresmerio00@gmail.com>
2023-11-25 21:21:43 -0300

    Programa Principal implementado

--- Account/src/Application/Program.java
+++ Account/src/Application/Program.java
@@ -1,10 +1,44 @@
 package Application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exception.DomainException;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		try { 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		}
+		catch (DomainException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
 	}
 
 }
